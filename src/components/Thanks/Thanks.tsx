bf6d5d086af599f27cc98cf8d7dc7ff96510e050
Kuba <wojtkiewicz.jakub@gmail.com>
2023-02-18 16:10:06 +0100

    thanks2

--- src/components/Thanks/Thanks.tsx
+++ src/components/Thanks/Thanks.tsx
@@ -4,7 +4,7 @@ const Thanks = () => {
   return (
     <div>
       
-<img style={{width:200, margin:"auto", alignContent:"center", display: "block"}} src= "https://previews.123rf.com/images/jovanas/jovanas1602/jovanas160201509/52197055-thank-you-ic%C3%B4ne-que.jpg"></img>
+<img style={{width:200, margin:"auto", alignContent:"center", display: "block"}} src= "https://cdn-icons-png.flaticon.com/512/5361/5361053.png"></img>
       </div>
     
   )
