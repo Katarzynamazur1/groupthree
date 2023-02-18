5300e06763c8ef6ec4bf89f7d7d07e740e0a284b
DESKTOP-ASVGKVJ\Paweł Sońta <naukafrontenddev@gmail.com>
2023-02-18 11:51:56 +0100

    18.02.2023 11.52

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar/Navbar";
 import { useState } from "react";
 import Location from "./components/Location/Location";
 import Weather from "./components/Weather/Weather";
+import Currency from "./components/Currency/Currency";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Currency />
       <Weather />
     </div>
   );
--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,7 +1,58 @@
+import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Currency = () => {
-  return <div>Currency cash</div>;
+  useEffect(() => {
+    axios
+      .get(`http://api.nbp.pl/api/exchangerates/rates/a/usd/?`)
+      .then((inCurrencyData) => console.log(inCurrencyData.data));
+  }, []);
+
+  //   const [finalCash, setFinalCash] = useState(0);
+  //   const inResponse = await fetch(
+  //     `http://api.nbp.pl/api/exchangerates/rates/a/chf/?`
+  //   );
+  //   const inData = await inResponse.json();
+  //   const outRespone = await fetch(
+  //     `http://api.nbp.pl/api/exchangerates/rates/a/usd/?`
+  //   );
+  //   const outData = await outRespone.json();
+  //   const inCurrent = await inData.rates[0].mid;
+  //   const outCurrent = await outData.rates[0].mid;
+  //   const calculs = await ((100 * inCurrent) / outCurrent);
+  //   setFinalCash(calculs);
+  return <div>{/* <p>{finalCash}</p> */}</div>;
 };
 
 export default Currency;
+
+// const Currency = async () => {
+//   //   const [finalCash, setFinalCash] = useState(0);
+//   const inCurrencyData: any = document.getElementById("kwotaIn");
+//   const outCurrencyData: any = document.getElementById("kwotaOut");
+//   const inAmount: any = document.getElementById("kwotaIn");
+//   //   const inResponse = await fetch(
+//   //     `http://api.nbp.pl/api/exchangerates/rates/a/chf/?`
+//   //   );
+//   //   const inData = await inResponse.json();
+//   //   const outRespone = await fetch(
+//   //     `http://api.nbp.pl/api/exchangerates/rates/a/usd/?`
+//   //   );
+//   //   const outData = await outRespone.json();
+//   //   const inCurrent = await inData.rates[0].mid;
+//   //   const outCurrent = await outData.rates[0].mid;
+//   const finalCash = (inAmount * inCurrencyData) / outCurrencyData;
+//   //   setFinalCash(finalCash);
+//   return (
+//     <div>
+//       <input placeholder="kwotaIn" id="kwotaIn"></input>
+//       <input placeholder="walutaIn" id="walutaIn"></input>
+//       <button>Przelicz</button>
+//       <p>{finalCash}</p>
+//       <input placeholder="walutaIn" id="walutaIn"></input>
+//     </div>
+//   );
+// };
+
+// export default Currency;
