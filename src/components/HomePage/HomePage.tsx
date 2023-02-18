d2d2fe96969a9e80ba4a977638c28a833ceee7a5
Grzegorz Wozniak <grubysweter@gmail.com>
2023-02-18 11:56:10 +0100

    justified HomePage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,31 +1,44 @@
-import React from 'react'; 
+import React from "react";
 import Typography from "@mui/material/Typography";
-import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
 import Button from "@mui/material/Button";
-import CoffeeIcon from '@mui/icons-material/Coffee';
-import { Link } from 'react-router-dom';
-import { Fab } from '@mui/material';
+import CoffeeIcon from "@mui/icons-material/Coffee";
+import { Link } from "react-router-dom";
+import { Fab } from "@mui/material";
 
 const HomePage = () => {
   return (
     <>
-        <Typography variant='h1' component='div' mt={2} sx={{fontSize:40, textAlign:"center"}}>
-Witaj w <br /> TWOIM kantorze
+      <Typography
+        align="center"
+        variant="h1"
+        component="div"
+        mt={2}
+        sx={{ fontSize: 40, textAlign: "center" }}
+      >
+        Witaj w <br /> TWOIM kantorze
+      </Typography>
+      <div style={{ alignContent: "center", margin: 20 }}>
+        <Typography align="center" variant="h2" sx={{ fontSize: 20, my: 2 }}>
+          <p> Exchange your money!</p>
+          <Link to="/">
+            <Fab color="success">
+              <CurrencyExchangeIcon />
+            </Fab>
+          </Link>
         </Typography>
-        <div style={{alignContent:"center", margin: 20}}>
-        <Typography variant='h2' sx={{fontSize:20, my:2}}>Exchange your money!</Typography>
-            <Link to='/'>
-                <Fab color="success" >
-                    <CurrencyExchangeIcon/>
-                </Fab>
-            </Link>
-        <Typography variant='h2' sx={{fontSize:20, my:2}}>Kup nam kawę :)</Typography>
-            <Link to='/Thanks'>
-                <Fab color="success"> <CoffeeIcon /> </Fab>
-            </Link>
-        </div>
-        </>
-  )
-}
 
-export default HomePage
+        <Typography align="center" variant="h2" sx={{ fontSize: 20, my: 2 }}>
+          <p> Kup nam kawę :)</p>
+          <Link to="/Thanks">
+            <Fab color="success">
+              <CoffeeIcon />
+            </Fab>
+          </Link>
+        </Typography>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
