9074877ae93904416301170f36d6235c8508a88b
Joanna <joanna.malgorzata@gmail.com>
2023-02-18 14:44:28 +0100

    home icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,18 +8,22 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 import { Link } from "react-router-dom";
-
+import OtherHousesIcon from '@mui/icons-material/OtherHouses';
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
+          <Link to='/' style={{ textDecoration: "none", color: "white"}}>
+          <OtherHousesIcon />
+          </Link>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
            Twój Kantor
           </Typography>
+          
           <Link to='/exchange' 
-          style={{ textDecoration: "none", color: "white" }}>
+          style={{textDecoration: "none", color: "white" }}>
           <Button color="inherit" startIcon={< CurrencyExchangeIcon />}>Exchange</Button >
           </Link>
         </Toolbar>
