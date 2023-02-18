aac350669d4cf4d36caa88ac34401c8aa45ebf6c
Kuba <wojtkiewicz.jakub@gmail.com>
2023-02-18 15:59:03 +0100

    thanks icon

--- src/components/Thanks/Thanks.tsx
+++ src/components/Thanks/Thanks.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Thanks = () => {
   return (
-    <div>Thanks</div>
+    <div>
+      
+<img style={{width:200, margin:"auto", alignContent:"center", display: "block"}} src= "https://previews.123rf.com/images/jovanas/jovanas1602/jovanas160201509/52197055-thank-you-ic%C3%B4ne-que.jpg"></img>
+      </div>
+    
   )
 }
 
